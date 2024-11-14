9038119560d5bfe54cf16e2a432ebabadfb1c0d2
m-hemma-k <hemma.kargl@gmail.com>
2024-11-14 15:37:12 +0100

    Update ini4.py

--- ini4.py
+++ ini4.py
@@ -2,9 +2,11 @@
 
 # Note: This script requires Python 3.12.7
 
+# Given: Two positive integers a and b (a < b < 10000).
 a = 4287
 b = 9006
 
+# Return: The sum of all odd integers from a through b, inclusively.
 sum_odd = 0
 
 for x in range(a, b + 1):
