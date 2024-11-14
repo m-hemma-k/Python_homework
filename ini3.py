f38779154665d5293466990863b77539022ab97e
m-hemma-k <hemma.kargl@gmail.com>
2024-11-14 14:19:54 +0100

    Add ini3 Rosalind homework

---
+++ ini3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Note: This script requires Python 3.12.7
+
+s = "b82RQeef71uXW7jlAeZe4A9IGekkoRPSIzAGrqmOdJFH2B2JHuiDHTHGRrLzx18XdbQ7A1SbHGZBQSu1ljbdagpBbFrI9jvLSecOdhFIjB7zQdhwtyVnnuptaDn5clNpsrPf3jcAWemngBZq12cHNSU6e2JiqfYkX8zALkquBN0N7KQO40fITYb."
+a = 24
+b = 28
+c = 116
+d = 120
+
+print(s[a:b+1], s[c:d+1])
