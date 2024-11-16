73405184675b33ca392f70e0c3ed3141fd8cdd41
m-hemma-k <hemma.kargl@gmail.com>
2024-11-16 12:03:08 +0100

    ignore all Rosalind txt files

--- Rosalind/ini2.py
+++ Rosalind/ini2.py
@@ -1,68 +1,10 @@
-# **Homework by Hemma Kargl**
-### Course: **2024W 300160-1 Practical introduction to programming for biologists**
-#### Section: Rosalind exercise INI2
-#### Date: 12.11.2024
+#!/usr/bin/env python3
+# Note: This script requires Python 3.12.7
 
-## **Rosalind exercise INI2**
-### Variables and Some Arithmetic
-```
-Problem
-Given: Two positive integers a and b, each less than 1000.
-
-Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.
-```
-
-### *My solution:*
-```
+# Given: Two positive integers a and b, each less than 1000.
 a = 938
 b = 981
+
+# Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.
+
 print(a**2 + b**2)
-```
-
-## **Python Slicing Exercises**
-
-### Exercise 17-a
-### Slice the word until first "a". (Tosc)
-```
-wrd="Toscana"
-#Type your answer here.
-
-ans_1=
-
-
-print(ans_1)
-```
-
-### *My solution:*
-```
-wrd="Toscana"
-#Type your answer here.
-
-ans_1=wrd[:-3]
-
-
-print(ans_1)
-```
-
-### Exercise 17-b
-### Slice the word so that you get "cana".
-```
-wrd="Toscana"
-#Type your answer here.
-
-ans_1=
-
-
-print(ans_1)
-```
-
-### *My solution:*
-```
-wrd="Toscana"
-#Type your answer here.
-
-ans_1=wrd[3:]
-
-
-print(ans_1)
-```
