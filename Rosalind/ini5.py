989c869a94907e3d017d765e18f80ff73682c698
m-hemma-k <hemma.kargl@gmail.com>
2024-11-16 14:43:20 +0100

    Commented the code for better understanding

--- Rosalind/ini5.py
+++ Rosalind/ini5.py
@@ -2,12 +2,17 @@
 
 # Note: This script requires Python 3.12.7
 
-# first, read the file!
+# Define the file path to the input file
 filepath = "./rosalind_data/rosalind_ini5.txt"
+# Open the file in read mode and assign it to 'infile'
 with open(filepath, 'r') as infile:
     lines = infile.readlines()
+    # Iterate over each line in the 'lines' list
     for line in lines:
+    # Strip whitespace from the beginning and end of each line
         (line.strip())
 
+# Iterate over every second line in the 'lines' list, starting from the second line (index 1)
 for l in lines[1::2]:
+    # Print each of these lines after stripping whitespace from the beginning and end
     print(l.strip())
