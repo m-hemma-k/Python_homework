b173557da0cef4abb4dd9cc79e0f53ffc34c13b8
m-hemma-k <hemma.kargl@gmail.com>
2024-11-18 10:18:44 +0100

    Add new version of ini5 and util function

--- Rosalind/ini5.py
+++ Rosalind/ini5.py
@@ -2,6 +2,12 @@
 
 # Note: This script requires Python 3.12.7
 
+from util import read_input
+# first, read the file!
+test = "./rosalind_data/rosalind_ini5.txt"
+lines = read_input(test)
+
+"""
 # Define the file path to the input file
 filepath = "./rosalind_data/rosalind_ini5.txt"
 # Open the file in read mode and assign it to 'infile'
@@ -11,6 +17,7 @@ with open(filepath, 'r') as infile:
     for line in lines:
     # Strip whitespace from the beginning and end of each line
         (line.strip())
+"""
 
 # Iterate over every second line in the 'lines' list, starting from the second line (index 1)
 for l in lines[1::2]:
---
+++ Rosalind/util.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# a library of utility functions for Rosalind exercises
+
+def read_input(filepath):
+    with open(filepath, 'r') as infile:
+        lines = infile.readlines()
+        stripped = []
+        for line in lines:
+            stripped.append(line.strip())
+    return stripped
