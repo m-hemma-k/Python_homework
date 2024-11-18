4ce1fdd293afff006c2cb182f0fd8a5db8b4d23d
m-hemma-k <hemma.kargl@gmail.com>
2024-11-18 10:45:40 +0100

    Updated RNA to a for loop

--- Rosalind/RNA.py
+++ Rosalind/RNA.py
@@ -4,6 +4,7 @@
 # PROBLEM: An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
 # Given a DNA string t corresponding to a coding strand, its transcribed RNA string u is formed by replacing all occurrences of 'T' in t with 'U' in u.
 
+"""
 # Given: A DNA string t having length at most 1000 nt.
 filepath = "./rosalind_data/rosalind_RNA.txt"
 
@@ -12,6 +13,20 @@ with open(filepath, 'r') as infile:
     # Read line from the file
     sequence = infile.readline().strip()
 
+
 # Return: The transcribed RNA string of t.
 RNA = sequence.replace("T", "U")
 print(RNA)
+"""
+
+sequence = 'GATGGAACTTGACTACGTAAATT'
+RNA = ''
+
+for nt in sequence:
+    if nt == 'T':
+        RNA += 'U'
+    else:
+        RNA += nt
+
+print(RNA)
+    
